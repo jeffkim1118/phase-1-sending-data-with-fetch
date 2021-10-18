79be11bce8d23e04f90201ce2c23c26ac99e8f52
Yoonsung <jeffkim1118@outlook.com>
2021-10-18 18:40:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+/*
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+fetch(`http://localhost:3000/dogs`, configurationObject)
+.then(function(response) {
+    return response.json();
+})
+.then(function(object) {
+    console.log(object)
+})
+.catch(function(error) {
+    alert("Bad things! Ragnarok!");
+    console.log(error.message);
+})
+*/
+const user = {
+    name: 'Steve',
+    email: 'steve@steve.com',
+};
+
+function submitData(){
+    fetch(`http://localhost:3000/users`,{
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(user),
+    })
+    .then((res)=>res.json)
+    .then((data)=> console.log(data))
+    .catch(function(error) {
+        let p = document.createElement('p')
+        body.append(error.message,p)
+    })
+}
+
+
+
+
+
+
